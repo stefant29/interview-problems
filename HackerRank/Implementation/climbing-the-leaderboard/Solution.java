15d568ae8626b2066b2958b98e41050e6b088810
Stefan Toncu <stefan.toncu29@gmail.com>
2017-07-11 11:13:50 +0300

    [hackerrank] - implementation - climbing-the-leaderboard 20/20

--- HackerRank/Implementation/climbing-the-leaderboard/Solution.java
+++ HackerRank/Implementation/climbing-the-leaderboard/Solution.java
@@ -4,46 +4,57 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-//https://www.hackerrank.com/challenges/climbing-the-leaderboard
 public class Solution {
+    public static int getLevel(int n, int[] scores, int currentScore) {
+        // extreme cases: first or last level
+        if (currentScore < scores[n-1])
+            return n + 1;
+        else if (currentScore > scores[0])
+            return 1;
+
+        // binary search algo for finding the place in the leaderboard
+        int lo = 0, hi = n-1, mid = -1;
+        while (lo <= hi) {
+            mid = lo + (hi-lo)/2;
+            // compare with middle element
+            if (scores[mid] == currentScore)
+                return mid + 1;
+            // if smaller, lower the high end
+            else if (scores[mid] < currentScore) 
+                hi = mid-1;
+            // if bigger, advance the low end
+            else
+                lo = mid+1;
+        }
+        // return the current position
+        return lo + 1;
+    }
 
     public static void main(String[] args) {
         // read from STDIN
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        // store the score in a linkedList (dynamically allocated for
-        //  a random number of elements: the number of elements is
-        //  smaller or equal to n because i don't store duplicates)
-        LinkedList<Integer> scores = new LinkedList<Integer>();
+        int[] scores = new int[n];
+        int actualDimension = 1;
         int lastElement = -1;
-        for(int scores_i=0; scores_i < n; scores_i++){
+        scores[0] = in.nextInt();
+        for(int scores_i=0; scores_i < n - 1; scores_i++){
             int currentElement = in.nextInt();
             // only if the element does not already exists
-            if (lastElement != currentElement) {
-                // add the element to the linkedlist
-                scores.addFirst(currentElement);
-                lastElement = currentElement;
+            if (scores[actualDimension-1] != currentElement) {
+                // add the element to the array
+                scores[actualDimension] = currentElement;
+                // update the dimension of the array
+                actualDimension++;
             }
         }
         
         // read the number of levels Alice is playing
         int m = in.nextInt();
-        for(int i=0; i < m; i++){
+        // for each level, compute the level she is currently on
+        for(int i=0; i < m; i++) {
             int currentScore = in.nextInt();
-            // if the current score is bigger than the first players's
-            // score, than Alice is at the top of the ladder
-            if (currentScore >= (int)scores.getLast()) {
-                System.out.println("1");
-                continue;
-            }
-            
-            // check the current level Alice is on
-            for (int j = 0; j < scores.size(); j++) {
-                if (currentScore < (int)scores.get(j)) {
-                    System.out.println(scores.size() - j + 1);
-                    break;
-                }
-            }
+            System.out.println(getLevel(actualDimension, scores, currentScore));
         }
     }
 }
