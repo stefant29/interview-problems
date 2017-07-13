d77657b82a4972cc7f87bf45a4a10ccb19e32d9c
Stefan Toncu <stefan.toncu29@gmail.com>
2017-07-13 19:15:28 +0300

    [hackerrank] - implementation - minimum-distances 9/9

--- HackerRank/Implementation/minimum-distances/Solution.java
+++ HackerRank/Implementation/minimum-distances/Solution.java
@@ -8,31 +8,38 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
+        // use scanner to read from STDIN
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int A[] = new int[n];
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-            // key,    value
         int max_app = Integer.MIN_VALUE;
-        for(int A_i=0; A_i < n; A_i++) {
+        for(int A_i=0; A_i < n; A_i++)
             A[A_i] = in.nextInt();
-            Integer count = map.get(A[A_i]);
-            if (count == null)
-                count = new Integer(0);
-            map.put(A[A_i], (int)count + 1);
-            if (count + 1 > max_app)
-                max_app = count + 1;
-        }
-        System.out.println("ma: " + max_app);
-        for (int i = 0; i < n; i++) {
-            if ((int)map.get(A[i]) == max_app) {
-                int first = i;
-                int last = -1;
-                while() {
-                    
+        
+        // find the minimum distance between two duplicates in the list
+        int min_dist = Integer.MAX_VALUE;
+        for(int i=0; i < n-1; i++) {
+            // find min_distance for each element
+            int current_dist = 0;
+            for(int j=i+1; j < n; j++) {
+                current_dist ++;
+                if (A[i] == A[j]) {
+                    // update the global minimum distance
+                    if (current_dist < min_dist)
+                        min_dist = current_dist;
+                    break;
                 }
-            }           
+            }
         }
-        System.out.println(map);
+        
+        // if min_dist was not modified, then there are no duplicates
+        //     in this case, print -1 to STDOUT
+        if (min_dist == Integer.MAX_VALUE)
+            System.out.println("-1");
+        else
+            System.out.println(min_dist);
+        
+        // close scanner
+        in.close();
     }
 }
