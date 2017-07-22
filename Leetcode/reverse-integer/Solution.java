d364ed06541525b0d209bc97b304b5df81e96c1a
Stefan Toncu <stefan.toncu29@gmail.com>
2017-07-22 22:59:17 +0300

    [leetcode] - algorithms - reverse-integer

---
+++ Leetcode/reverse-integer/Solution.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/reverse-integer/#/description
+public class Solution {
+    public int reverse(int x) {
+        long rev = 0;
+        while (x != 0) {
+            rev = rev * 10 + x % 10;
+            if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE)
+                return 0;
+            x /= 10;
+        }
+        return (int)rev;
+    }
+}
