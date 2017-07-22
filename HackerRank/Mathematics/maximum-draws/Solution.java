6a4a13914d4905f9e2f39f896ab95437ed1efb30
Stefan Toncu <stefan.toncu29@gmail.com>
2017-07-22 23:30:24 +0300

    [hackerrank] - fundamentals - maximum-draws

---
+++ HackerRank/Mathematics/maximum-draws/Solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+// https://www.hackerrank.com/challenges/maximum-draws
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int test_cases = in.nextInt();
+        for (int test = 0; test < test_cases; test++) {
+            int no_socks = in.nextInt();
+            System.out.println(no_socks + 1);
+        }
+    }
+}
