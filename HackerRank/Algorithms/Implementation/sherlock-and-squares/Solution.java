cd5b1ae8a8e5c5473fd5a381317774c0a1e3e14e
Stefan Toncu <stefan.toncu29@gmail.com>
2017-08-14 16:09:21 +0300

    [hackerrank] - implementation - sherlock-and-squares - 100%

--- HackerRank/Algorithms/Implementation/sherlock-and-squares/Solution.java
+++ HackerRank/Algorithms/Implementation/sherlock-and-squares/Solution.java
@@ -4,29 +4,29 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+// https://www.hackerrank.com/challenges/sherlock-and-squares
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        /* read input using a scanner */
         Scanner in  = new Scanner(System.in);
         int tests = in.nextInt();
         for (int test = 0; test < tests; test++) {
+            /* read low limit and upper limit */
             int A = in.nextInt();
             int B = in.nextInt();
+            /* find roots of both limits */
             int rootA = (int)Math.sqrt(A);
             int rootB = (int)Math.sqrt(B);
-            if (rootA==rootB && rootA*rootA == A) {
-                System.out.println("1");
-                continue;
-            }
+            /* find the number of perfect squares in between */
             int result = rootB-rootA;
-            if (result == 0) {
-                if (rootA*rootA == A)
-                    result++;
-                if (rootB*rootB == B)
-                    result++;
-            }
+            /* check if the low limit is also a perfect square */
+            if (rootA*rootA == A)
+                result++;
+            /* print result to STDOUT */
             System.out.println(result);
         }
+        /* close scanner */
+        in.close();
     }
 }
