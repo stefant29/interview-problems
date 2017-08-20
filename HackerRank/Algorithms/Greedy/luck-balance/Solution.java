70a6df3359ab56a9117808b2d7b8efc43c3db9d6
Stefan Toncu <stefan.toncu29@gmail.com>
2017-08-20 12:57:34 +0300

    [hackerrank] - Greedy - luck-balance - better space complexity

--- HackerRank/Algorithms/Greedy/luck-balance/Solution.java
+++ HackerRank/Algorithms/Greedy/luck-balance/Solution.java
@@ -3,71 +3,43 @@ import java.util.*;
 // https://www.hackerrank.com/challenges/luck-balance
 public class Solution {
 
-    /* aditional class which stores a contest */
-    static class Contest {
-        private int luck;
-        private boolean important;
-        
-        /* constructor */
-        public Contest(int luck, boolean important) {
-            this.luck = luck;
-            this.important = important;
-        }
-        
-        /* getter for luck */
-        public int getLuck() {
-        	return luck;
-        }        
-    }
-    
     public static void main(String[] args) {
         /* read input */
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
         int K = in.nextInt();
-        /* 
-         * use two aditional data structures to store 
-         * the important and unimportant contests
-         */
-        ArrayList<Contest> c1 = new ArrayList<Contest>();
-        ArrayList<Contest> c2 = new ArrayList<Contest>();
+
+        ArrayList<Integer> c1 = new ArrayList<Integer>();
+        ArrayList<Integer> c2 = new ArrayList<Integer>();
         for (int contest = 0; contest < N; contest++) {
-            /* read data for each contest */
             int L = in.nextInt();
             int T = in.nextInt();
-            /* add the input into a new Contest */
             if (T == 1)
-                c1.add(new Contest(L, true));
+            		c1.add(L);
             else
-                c2.add(new Contest(L, false));
+            		c2.add(L);
         }
         
-        /*
-         * greedy approach: lose the contests with the biggest luck:
-         * sort the contests by luck, then lose the K luckiest contests,
-         * to increase the luck as much as possible
-         */
-        c1.sort(Comparator.comparing(Contest::getLuck).reversed());
+        c1.sort(new Comparator<Integer>() {
+        		public int compare(Integer o1, Integer o2) {
+        			return o1 - o2;
+        		}
+        }.reversed());
         
         int totalLuck = 0;
-        /* important contests: Leena must not lose more than K contests */
-        for (int i = 0; i < c1.size(); i++) {
-            /* Lena looses K contests; add their luck */
-            if (K > 0) {
-                totalLuck += c1.get(i).getLuck();
-                K--;
-            /* Lena is forced to win the remaining contests; substract their luck */
-            } else
-                totalLuck -= c1.get(i).getLuck();
-        }
-        /* these contest are not important, Lena can lose them; add their luck*/
+        for (int i = 0; i < c1.size(); i++)
+            if (K-- > 0)
+                totalLuck += c1.get(i);
+            else
+                totalLuck -= c1.get(i);
+
         for (int i = 0; i < c2.size(); i++)
-        		totalLuck += c2.get(i).getLuck();
+        		totalLuck += c2.get(i);
         
-        /* print total luck */
         System.out.println(totalLuck);
         
-        /* close scanner */
         in.close();
     }
 }
+
+
